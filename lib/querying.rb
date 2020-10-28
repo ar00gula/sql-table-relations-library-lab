88e2b8d2859c4f602e1a56ac58cdc1773f46351a
ar00gula <arianna.mayer@gmail.com>
2020-10-28 12:19:57 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,48 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year
+  FROM Books
+  INNER JOIN Series
+  ON Series.id = Books.series_id
+  WHERE Series.id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT Characters.name, Characters.motto
+  FROM Characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
-end
+  "SELECT species, COUNT(Characters.species)
+  FROM Characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1"
+end #i don't understand why this worked and the same thing without group by species returned a value i don't think existed?
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT Authors.name, Subgenres.name
+  FROM Authors
+  INNER JOIN Subgenres, Series
+  ON Authors.id = Series.author_id WHERE Series.subgenre_id = Subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT Series.title
+  FROM Series
+  LEFT OUTER JOIN character_books, books, characters
+  ON Series.id = Books.series_id WHERE Books.id = character_books.book_id
+  GROUP BY Characters.species HAVING Characters.species = 'human'
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT Characters.name, COUNT(character_books.book_id)
+  FROM Characters
+  LEFT OUTER JOIN character_books
+  ON Characters.id = character_books.character_id
+  GROUP BY Characters.name
+  ORDER BY COUNT(character_books.book_id) DESC, Characters.name ASC"
 end
